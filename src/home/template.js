05abb126db697fc4c2e512f1f6796dead2806cc1
Keily Salinas <keisalinas2@gmail.com>
2016-12-26 22:56:30 -0400

    add timeline

--- src/home/template.js
+++ src/home/template.js
@@ -1,3 +1,12 @@
 var yo = require('yo-yo');
+var layout = require('../layout')
 
-module.exports = yo `<a href="/signup"> Signup </a>`;
+var template = yo `<div class="container timeline">
+  <div class="row">
+    <div class="col s12 m10 offset-m1 l6 offset-l3">
+      content
+    </div>
+  </div>
+ </div>`;
+
+module.exports = layout(template);
